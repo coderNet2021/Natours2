9f8a56fa33bc53d34d641f8d94267ea84268c803
Lilyane Fawaz <cnss.saida@gmail.com>
2021-08-05 00:26:57 +0300

    commiting

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreatedIndex: true,
+    useFindAndModify: false
+  })
+  .then(con => {
+    console.log(con.connections);
+    console.log('DB connected!');
+  });
+
+// const testTour = new TourTest({
+//   name: 'test tour from mac no price',
+// });
+
+// testTour
+//   .save()
+//   .then(doc => {
+//     console.log(doc);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

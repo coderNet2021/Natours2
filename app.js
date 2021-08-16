258f83679852a92662715adb67bc5b8dac2e09f2
Lilyane Fawaz <cnss.saida@gmail.com>
2021-08-16 09:49:02 +0300

    error handling 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
-const globalErrorHandler=require('./controllers/errorController');
+const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -32,7 +32,7 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
   // res.status(404).json({
   //   status: 'fail',
   //   message: `cant find url ${req.originalUrl} on the server!`
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,8 +4,19 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      stack: err.stack,
+      error: err
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+      //stack:err.stack,
+      //error : err
+    });
+  }
 };

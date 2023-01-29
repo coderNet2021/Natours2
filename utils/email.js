f60e07f6baf490eda08cfc1a721ab2a7e287a5c7
lilyane2021 <liliane.fawaz@gmail.com>
2023-01-29 04:44:12 +0100

    changing email to test@test.com

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async options => {
 
   // 2) Define the email options
   const mailOptions = {
-    from: 'lilyane fawaz <liliane.fawaz@gmail.com>',
+    from: 'l f <test@test.com>',
     to: options.email,
     subject: options.subject,
     text: options.message

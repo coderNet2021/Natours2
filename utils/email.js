c7c1715fa9db1f93b5fbb6ba1d7edbc5a1c7c3bb
coderNet2021 <cnss.saida@gmail.com>
2023-01-29 04:52:47 +0100

    test

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async options => {
 
   // 2) Define the email options
   const mailOptions = {
-    from: 'l f <test@test.com>',
+    from: 'l ff <test@test.com>',
     to: options.email,
     subject: options.subject,
     text: options.message
